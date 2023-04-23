f460133526995aec7ddc693e1b2a86e0da0e5312
roosleex <roosleex@gmail.com>
2023-04-23 21:35:12 +0300

    Add Rank

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/*<FaceRecognition />*/}
     </div>
